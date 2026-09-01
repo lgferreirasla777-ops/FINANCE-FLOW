a811e7780d8cb1588ca9eaf24de9a56b488b6047
lgferreirasla777-ops <lgferreirasla777@gmail.com>
2026-09-01 09:08:44 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FinanceFlowApp from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FinanceFlowApp />
-  </React.StrictMode>
-);
